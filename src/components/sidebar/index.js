3770fddacdacc5732519e7b834f43be797f80e9c
Georgiy Kapustin <84815448+inspiredx@users.noreply.github.com>
2022-11-07 07:25:33 +0300

    Added Spotify API

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './sidebar.css'
 import SidebarButton from './sidebarButton.js';
 import { MdFavorite } from "react-icons/md";
@@ -6,13 +6,20 @@ import { FaGripfire, FaPlay } from "react-icons/fa";
 import { FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import { MdSpaceDashboard } from "react-icons/md";
-
+import apiClient from '../../spotify';
 
 
 export default function Sidebar() {
+
+  const [image, setImage] = useState('https://sun9-74.userapi.com/impg/c857220/v857220949/de4a5/yE4ZGYUf55w.jpg?size=608x1080&quality=96&sign=e73e18f169e9b58b9f44a71d9f84ba90&type=album');
+  useEffect(() => {
+    apiClient.get("me").then(response => {
+      setImage(response.data.images[0].url);
+    });
+  },[]);
   return ( 
   <div className='sidebar-container'>
-    <img src='https://sun9-74.userapi.com/impg/c857220/v857220949/de4a5/yE4ZGYUf55w.jpg?size=608x1080&quality=96&sign=e73e18f169e9b58b9f44a71d9f84ba90&type=album' className='profile-img' alt='Profile Img' />
+    <img src= {image} className='profile-img' alt='Profile Img' />
     <div>
         <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
         <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Library from "../library/library"
-import Feed from "../feed/feed"
-import Trending from '../trending/trending'
-import Player from "../player/player"
-import Favorites from "../favorites/favorites"
-import "./home.css"
-import Sidebar from '../../components/sidebar'
+import React, { useEffect, useState } from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "../auth/login";
+import Library from "../library/library";
+import Feed from "../feed/feed";
+import Trending from '../trending/trending';
+import Player from "../player/player";
+import Favorites from "../favorites/favorites";
+import "./home.css";
+import Sidebar from '../../components/sidebar';
+import { setClientToken } from '../../spotify';
 
 export default function Home() {
-  return (
-         
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    if(!token && hash) {
+    const _token = hash.split("&")[0].split("=")[1];
+    window.localStorage.setItem("token", _token);
+    setToken(_token);
+    setClientToken(_token);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }    
+  }, []);
+
+  return !token ? 
+  (<Login />) : (       
     <Router>
-      <div className='main-body'> 
+      <div className='main-body'>      
     <Sidebar />
      <Routes>
         <Route path="/" element={<Library />} />
@@ -22,7 +42,6 @@ export default function Home() {
         <Route path="/favorites" element={<Favorites />} />
      </Routes>
      </div>
-    </Router>
-    
-  )
+    </Router>    
+  );
 }
