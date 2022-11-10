ae456c59e10fb48f2547f75ea144d94dc286ede9
Georgiy Kapustin <georgiykapustinx@gmail.com>
2022-11-10 04:34:08 +0300

    Added library styles

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../auth/login";
 import Library from "../library/library";
 import Feed from "../feed/feed";
@@ -18,30 +18,35 @@ export default function Home() {
     const token = window.localStorage.getItem("token");
     const hash = window.location.hash;
     window.location.hash = "";
-    if(!token && hash) {
-    const _token = hash.split("&")[0].split("=")[1];
-    window.localStorage.setItem("token", _token);
-    setToken(_token);
-    setClientToken(_token);
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", _token);
+      setToken(_token);
+      setClientToken(_token);
     } else {
       setToken(token);
       setClientToken(token);
-    }    
+    }
   }, []);
 
-  return !token ? 
-  (<Login />) : (       
-    <Router>
-      <div className='main-body'>      
-    <Sidebar />
-     <Routes>
-        <Route path="/" element={<Library />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/trending" element={<Trending />} />
-        <Route path="/player" element={<Player />} />
-        <Route path="/favorites" element={<Favorites />} />
-     </Routes>
-     </div>
-    </Router>    
-  );
+  window.addEventListener('beforeunload', function (e) {    
+    e.preventDefault();
+    return localStorage.clear();       
+  });
+
+  return !token ?
+    (<Login />) : (
+      <Router>
+        <div className='main-body'>
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Library />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/trending" element={<Trending />} />
+            <Route path="/player" element={<Player />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </div>
+      </Router>
+    );
 }
--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,29 +1,43 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { IconContext } from 'react-icons';
+import { AiFillPlayCircle } from 'react-icons/ai'
 import APIkit from '../../spotify';
 import './library.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function Library() {
 
   const [playlists, setPlaylists] = useState(null);
 
   useEffect(() => {
-    APIkit.get('me/playlists').then(function(response) {
-    setPlaylists(response.data.items);
-    console.log(response.data.items);
+    APIkit.get('me/playlists').then(function (response) {
+      setPlaylists(response.data.items);
+      console.log(response.data.items);
     });
-  }, []); 
+  }, []);
+
+  const navigate = useNavigate();
+
+  const playPlaylist = (id) => {
+    navigate('/player', { state: { id: id } });
+  }
 
   return (
-  <div className='screen-container'>
-    <div className='library-body'>
-    {playlists?.map((playlist => 
-      <div className='playlist-card'>
-        <img src={playlist.images[0].url} className='playlist-image' alt='Playlist Pic' />
-        <p className='playlist-title'>{playlist.name}</p>
-        <p className='playlist-total'>{playlist.tracks.total} songs</p>
+    <div className='screen-container'>
+      <div className='library-body'>
+        {playlists?.map((playlist =>
+          <div className='playlist-card' key={playlist.id} onClick={() => playPlaylist(playlist.id)}>
+            <img src={playlist.images[0].url} className='playlist-image' alt='Playlist Pic' />
+            <p className='playlist-title'>{playlist.name}</p>
+            <p className='playlist-total'>{playlist.tracks.total} songs</p>
+            <div className='playlist-fade'>
+              <IconContext.Provider value={{ size: "50px", color: "#E99D72" }}>
+                <AiFillPlayCircle />
+              </IconContext.Provider>
+            </div>
+          </div>
+        ))}
       </div>
-    ))}
-    </div>  
-  </div>
+    </div>
   );
 }
